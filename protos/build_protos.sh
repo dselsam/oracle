66e7a041c505861212232c58de421368d9d8bba6
Daniel Selsam <daniel.selsam@protonmail.com>
2020-10-01 08:03:22 -0700

    feat: basic protos for hask <-> py

---
+++ protos/build_protos.sh
@@ -0,0 +1,12 @@
+PYTHON_OUT=python/
+HASKELL_OUT=src/
+PROTOS=protos
+
+protoc --plugin=protoc-gen-haskell=`which proto-lens-protoc` \
+       --python_out="$PYTHON_OUT" \
+       --haskell_out="$HASKELL_OUT" \
+       "$PROTOS"/Embeddable.proto \
+       "$PROTOS"/ChoicePoint.proto \
+       "$PROTOS"/Result.proto \
+       "$PROTOS"/DataPoint.proto \
+       "$PROTOS"/Command.proto
