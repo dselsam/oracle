b5cb453f608376c89168de07c97240dd1196443a
Daniel Selsam <daniel.selsam@protonmail.com>
2020-10-01 20:17:54 -0700

    feat: interactive search

---
+++ src/Oracle/Search/Interactive.hs
@@ -0,0 +1,56 @@
+{-
+Copyright (c) 2020 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Daniel Selsam
+
+Replay search from list of choice indices.
+-}
+
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Oracle.Search.Interactive where
+
+import Oracle.Data.Embeddable
+import Oracle.Control.Monad.Search
+
+import Oracle.Search.Result (Result(Result))
+import qualified Oracle.Search.Result as Result
+
+import Oracle.Search.Trace (Trace(Trace))
+import qualified Oracle.Search.Trace as Trace
+
+import Oracle.Search.Task (Task(Task))
+import qualified Oracle.Search.Task as Task
+
+import Oracle.Search.Decision (Decision(Decision))
+import qualified Oracle.Search.Decision as Decision
+
+import Data.Vector (Vector)
+import qualified Data.Vector as Vector
+
+import Oracle.Data.Deque (Deque)
+import qualified Oracle.Data.Deque as Deque
+
+import Debug.Trace (traceM)
+import Data.Foldable (for_)
+import Control.Monad.Trans (lift, liftIO, MonadIO)
+import Control.Monad.Reader (ReaderT, runReaderT, ask, asks)
+import Control.Monad.State (StateT, evalStateT, get, gets, modify, State, execState)
+
+type Oracle m a = ChoicePoint m a -> m (Vector Float)
+
+interactive :: (Monad m, MonadIO m) => [(String, Oracle m a)] -> SearchT m a -> m (Maybe a)
+interactive oracles (SearchT k) = k >>= \case
+  Fail   -> pure Nothing
+  Done x -> pure (Just x)
+
+  Choice cp@(ChoicePoint snapshot cs) -> do
+    liftIO $ print (snapshot, fmap fst cs)
+    for_ oracles $ \(name, oracle) -> do
+      pi <- oracle cp
+      liftIO . putStrLn $ "  - " ++ name ++ ": " ++ show pi
+    user <- liftIO getLine
+    let i = (read user :: Int)
+    interactive oracles (snd $ cs Vector.! i)
