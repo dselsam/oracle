01d62cadb92982b9a22144cd965b09e313dacc65
Daniel Selsam <daniel.selsam@protonmail.com>
2020-10-01 19:44:39 -0700

    chore: missing headers

--- src/Oracle/Data/Grid/Grid.hs
+++ src/Oracle/Data/Grid/Grid.hs
@@ -1,3 +1,9 @@
+{-
+Copyright (c) 2020 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Daniel Selsam
+-}
+
 module Oracle.Data.Grid.Grid where
 
 import Control.Monad (guard)
